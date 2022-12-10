25775f4393fe1a7f92df30334082624014202a21
Dhaniel Sales <dhanielr94@gmail.com>
2022-12-10 14:42:39 -0300

    intial commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-import { Cart } from '../modules/Cart';
+import { Home } from '../modules/Home';
 import { Catalog } from '../modules/Catalog';
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Cart />} />
-        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/" element={<Home />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
